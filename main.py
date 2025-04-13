2ba0aa80a2247c980513011f17d60be69e4ff827
atokhix <132080770+atokhix@users.noreply.github.com>
2025-04-13 21:22:49 +0200

    nestedlist

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-hellopy=[print("hello") for x in range(100)]
+nested_list = [[x, x + 1] for x in range(100)]
+
+for pair in nested_list:
+    for item in pair:
+        print(f" {pair} and -> {item}")
